43b53ecfcfbf9a863705c46f3d9bf38c8f9843a9
Yash Baghel <yashbaghel005@gmail.com>
2026-03-15 11:58:12 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (85.51%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     long long countCommas(long long n) {
-        if(n<1000){
-            return 0;
-        }
-        if(n<1000000){
-            return n-1000+1;
-        }
         long long ans = 0;
-       ans += (1000000LL-1000)*1;
-        if(n<1000000000){
-            return ans+(n-1000000+1)*2;
+        long long start = 1000;
+        int commas = 1;
+
+        while (start <= n) {
+            long long end = start * 1000 - 1;
+            long long upto = min(n, end);
+
+            ans += (upto - start + 1) * commas;
+
+            start *= 1000;
+            commas++;
         }
-        ans+=(1000000000LL-1000000LL)*2;
-        if(n<1000000000000){
-            return ans+(n-1000000000+1)*3;
-        }
-        ans += (1000000000000LL-1000000000LL)*3;
-        if(n<1000000000000000LL){
-            return ans+(n-1000000000000+1)*4;
-        }
-        ans+=(1000000000000000LL-1000000000000LL)*4;
-        return ans+(n-1000000000000000LL+1)*5;
+
+        return ans;
     }
 };
