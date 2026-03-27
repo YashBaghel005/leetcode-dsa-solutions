b4bf7eb24141527c97d1477751f7d23484ed4a1c
Yash Baghel <yashbaghel005@gmail.com>
2026-03-27 07:32:36 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (80.07%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -6,7 +6,7 @@ public:
         for(int i = 0; i<nums.size(); i++){
             sum+=nums[i];
             ans = max(ans,sum);
-            if(sum<0){
+            if(sum < 0){
                 sum = 0;
             }
         }
