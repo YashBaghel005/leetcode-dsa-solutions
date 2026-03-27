1753c6ad37586488f7ef476a1036af52fd4e7ae3
Yash Baghel <yashbaghel005@gmail.com>
2026-03-27 08:18:25 +0530

    Time: 3 ms (66.44%) | Memory: 11 MB (39.81%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        
+        if(k == 0){
+            int a = 0;
+            int b = 0;
+            for(int i = 0; i<s.size()-1; i++){
+                if(s[i] == s[i+1]){
+                    a++;
+                }else{
+                    a = 0;
+                }
+                b = max(b,a);
+            }
+            return b+1;
+        }
         unordered_map<char,int> mp;
         int i = 0;
         int ans = 0;
-        for(int j = 0; j<s.size() && j>=i; j++){
+        int max_freq = 0;
+        for(int j = 0; j<s.size(); j++){
             mp[s[j]]++;
-            char max_key = '.';
-            int max_value = 0;
-            for(auto &[key,value] : mp){
-                if(max_value < value){
-                    max_key = key;
-                    max_value = value;
-                }
+            max_freq = max(max_freq,mp[s[j]]);
+            while((j-i+1)-max_freq > k){
+                mp[s[i]]--;
+                i++;
             }
-            if((j-i+1)-max_value <= k){
-                ans = max(ans,j-i+1);
-            }else{
-                int maxi_value = max_value;
-                while((j-i+1)-maxi_value > k){
-                    mp[s[i]]--;
-                    i++;
-                    char maxi_key = '.';
-                    maxi_value = 0;
-                    for(auto &[key,value] : mp){
-                        if(maxi_value < value){
-                            maxi_key = key;
-                            maxi_value = value;
-                        }
-                    }
-                }
-                ans = max(ans,j-i+1);
-                
-            }
+            ans = max(ans,j-i+1);
         }
         return ans;
     }
