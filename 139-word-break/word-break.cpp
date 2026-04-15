5c915419b610ef72ea83e7d16a9bb1429dbfb2ca
Yash Baghel <yashbaghel005@gmail.com>
2026-04-15 18:02:02 +0530

    Time: 7 ms (58.17%) | Memory: 17 MB (26.41%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    unordered_map<string, bool> dp;
-    bool solve(string s,vector<string>& wordDict,string t){
+    unordered_map<string,bool> mp;
+    bool solve(string s, vector<string>& wordDict,string t){
         if(s == t){
             return true;
         }if(s.size() < t.size()){
             return false;
         }
-        if (dp.count(t)) return dp[t];
+        if (mp.count(t)) return mp[t];
         bool ans = false;
         for(int i = 0; i<wordDict.size(); i++){
-            string next = t + wordDict[i];
-            if (s.substr(0, next.size()) != next) continue;
-            ans = ans||solve(s,wordDict,t+wordDict[i]);
-            if (ans) break; 
+            string next = t+wordDict[i];
+            if(s.substr(0,next.size()) != next) continue;
+            ans = ans || solve(s,wordDict,t+wordDict[i]);
         }
-        return dp[t] = ans;
+        return mp[t] = ans;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        
         return solve(s,wordDict,"");
     }
 };
