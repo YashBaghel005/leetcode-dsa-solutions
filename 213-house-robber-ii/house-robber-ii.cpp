458e29bb16297a131b393823abad87a46df3ed6b
Yash Baghel <yashbaghel005@gmail.com>
2026-07-27 11:19:28 +0530

    Time: 3 ms (6.69%) | Memory: 11.3 MB (7.71%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int solve(vector<int>& nums,int index,int n,int ft,vector<vector<int>> &dp){
-        if(index >= n){
+    int solve(vector<int>& nums,int idx,bool first,vector<vector<int>> &dp){
+        if(first && idx >= nums.size()-1){
             return 0;
         }
-        if(ft == 1 && index == n-1){
+        if(idx >= nums.size()){
             return 0;
+        } 
+        if(dp[idx][first] != -1){
+            return dp[idx][first];
         }
-        if(dp[index][ft] != -1){
-            return dp[index][ft];
-        }
-        int profit = 0;
-        if(index == 0){
-            profit += max(nums[index]+solve(nums,index+2,n,1,dp),solve(nums,index+1,n,0,dp));
-        }else
-            profit = max(nums[index]+solve(nums,index+2,n,ft,dp),solve(nums,index+1,n,ft,dp));
-
-        return dp[index][ft] = profit;
+        int a = nums[idx] + solve(nums,idx+2,first,dp);
+        int b = solve(nums,idx+1,first,dp);
+        return dp[idx][first] = max(a,b);
     }
     int rob(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size(),vector<int>(2,-1));
-        return solve(nums,0,nums.size(),0,dp);
+        if(nums.size() == 1){
+            return nums[0];
+        }
+        vector<vector<int>> dp(nums.size(),vector<int> (2,-1));
+        return max(solve(nums,0,true,dp),solve(nums,1,false,dp));
     }
 };
