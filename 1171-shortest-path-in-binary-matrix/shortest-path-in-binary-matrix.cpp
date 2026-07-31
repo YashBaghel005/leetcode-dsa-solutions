ad24713236ffd0d9c373d7841ea918df55cd4fa9
Yash Baghel <yashbaghel005@gmail.com>
2026-07-31 17:27:01 +0530

    Time: 109 ms (8.09%) | Memory: 22.1 MB (99.61%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        if(n == 1){
+            if(grid[0][0] == 0){
+                return 1;
+            }else{
+                return -1;
+            }
+        }
+        vector<vector<int>> dirs = {{1,0},{-1,0},{0,1},{0,-1},{1,1},{-1,1},{1,-1},{-1,-1}};
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
+        pq.push({1,{0,0}});
+        while(!pq.empty()){
+            auto t = pq.top();
+            pq.pop();
+            int p = t.first;
+            int x = t.second.first;
+            int y = t.second.second;
+            if(x == n-1 && y == n-1){
+                return p;
+            }
+            if(grid[x][y] == 1){
+                continue;
+            }
+            grid[x][y] = 1;
+            for(int i = 0; i<dirs.size(); i++){
+                int x1 = x + dirs[i][0];
+                int y1 = y + dirs[i][1];
+
+                if(x1 < 0 || x1 >= n || y1 < 0 || y1 >= n){
+                    continue;
+                }
+                if(grid[x1][y1] == 0){
+                    pq.push({1+p,{x1,y1}});
+                }
+            }
+        }
+        return -1;
+    }
+};
