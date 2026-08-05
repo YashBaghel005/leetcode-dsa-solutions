755dc8eed0f219db777139de44513c3f08ce44e9
Yash Baghel <yashbaghel005@gmail.com>
2026-08-05 05:35:34 +0530

    Time: 91 ms (9.00%) | Memory: 22.1 MB (99.65%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -3,15 +3,14 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
         if(n == 1){
-            if(grid[0][0] == 0){
-                return 1;
-            }else{
+            if(grid[0][0] == 1)
                 return -1;
-            }
+
+            return 1;
         }
-        vector<vector<int>> dirs = {{1,0},{-1,0},{0,1},{0,-1},{1,1},{-1,-1},{-1,1},{1,-1}};
+        vector<vector<int>> dirs = {{1,0},{0,1},{-1,0},{0,-1},{1,1},{-1,-1},{-1,1},{1,-1}};
         priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
-        pq.push({0,{0,0}});
+        pq.push({1,{0,0}});
         while(!pq.empty()){
             auto t = pq.top();
             pq.pop();
@@ -19,21 +18,23 @@ public:
             int x = t.second.first;
             int y = t.second.second;
             if(x == n-1 && y == n-1){
-                return path+1;
+                return path;
             }
             if(grid[x][y] == 1){
                 continue;
             }
             grid[x][y] = 1;
             for(int i = 0; i<dirs.size(); i++){
-                int x1 = x + dirs[i][0];
-                int y1 = y + dirs[i][1];
-                if(x1 >= n || x1 < 0 || y1 >= n || y1 < 0){
+                int x1 = x+dirs[i][0];
+                int y1 = y+dirs[i][1];
+
+                if(x1 < 0 || x1 >=n || y1 < 0 || y1 >=n){
                     continue;
                 }
-                if(grid[x1][y1] == 0){
-                    pq.push({path+1,{x1,y1}});
+                if(grid[x1][y1] == 1){
+                    continue;
                 }
+                pq.push({path+1,{x1,y1}});
             }
         }
         return -1;
