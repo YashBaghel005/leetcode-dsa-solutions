83abc256dd2d6f7acdc7b48770555fea004bd4fd
Yash Baghel <yashbaghel005@gmail.com>
2026-08-07 11:09:18 +0530

    Time: 9 ms (44.44%) | Memory: 13.2 MB (61.44%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
-        int n = grid.size();
+        vector<vector<int>> dirs = {{-1,0},{0,-1},{0,1},{1,0}};
         priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
-        pq.push({0,{0,0}});
-        vector<vector<bool>> vis(n,vector<bool>(n,false));
-        vector<vector<int>> dirs = {{1,0},{0,1},{-1,0},{0,-1}};
+        pq.push({grid[0][0],{0,0}});
+        vector<vector<bool>> vis(grid.size(),vector<bool>(grid[0].size(),0));
         while(!pq.empty()){
             auto t = pq.top();
             pq.pop();
-            int val = t.first;
+            int time = t.first;
             int x = t.second.first;
             int y = t.second.second;
-            if(x == n-1 && y == n-1){
-                return max(val,grid[x][y]);
+            if(x == grid.size()-1 && y == grid.size()-1){
+                return time;
             }
             if(vis[x][y]){
                 continue;
             }
             vis[x][y] = true;
-            for(int i = 0;i<dirs.size(); i++){
+            for(int i = 0; i<dirs.size(); i++){
                 int x1 = x+dirs[i][0];
                 int y1 = y+dirs[i][1];
-                if(x1 < 0 || x1 >= n || y1 < 0 || y1 >= n){
+                if(x1 >= grid.size() || x1 < 0 || y1 >= grid[0].size() || y1 < 0){
                     continue;
                 }
-                pq.push({max(grid[x][y],val),{x1,y1}});
+                if(vis[x1][y1]){
+                    continue;
+                }
+                pq.push({max(time,grid[x1][y1]),{x1,y1}});
             }
         }
         return -1;
