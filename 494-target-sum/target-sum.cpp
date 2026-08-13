8ea3ba4726eacf3a0d50b1261f4e8e229e01dd8b
Yash Baghel <yashbaghel005@gmail.com>
2026-08-13 07:35:16 +0530

    Time: 164 ms (31.47%) | Memory: 318.7 MB (5.02%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int target,int n,vector<vector<int>> &dp){
-        if(n == 0 && target == 0){
-            return 1;
-        }
-        else if(n == 0){
+    int solve(vector<int> &nums,int target,int idx,vector<vector<int>> &dp){
+        if(idx >= nums.size()){
+            if(target == 0){
+                return 1;
+            }
             return 0;
         }
-        if(target+1000>2000 || target+1000<0){
-            return 0;
-        }
-        if(dp[n][target+1000] != -1){
-            return dp[n][target+1000];
-        } 
-        int a = solve(nums,target-nums[n-1],n-1,dp);
-        int b = solve(nums,target+nums[n-1],n-1,dp);
 
-        return dp[n][target+1000] = a+b;
+        if(dp[idx][20000+target] != -1){
+            return dp[idx][20000+target];
+        }
+
+        int a = solve(nums,target+nums[idx],idx+1,dp);
+        int b = solve(nums,target-nums[idx],idx+1,dp);
+        return dp[idx][20000+target] = a+b;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<vector<int>> dp(n+1,vector<int> (2001,-1));
-        int ans = solve(nums,target,n,dp);
-        return ans;
+        vector<vector<int>> dp(nums.size()+1,vector<int>(41001,-1));
+        return solve(nums,target,0,dp);
     }
 };
