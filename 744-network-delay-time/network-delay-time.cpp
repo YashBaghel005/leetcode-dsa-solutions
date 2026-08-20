f45707ddc8b1adc06d25702c2bdcb7865c3773dd
Yash Baghel <yashbaghel005@gmail.com>
2026-08-20 19:25:11 +0530

    Time: 92 ms (29.14%) | Memory: 46.3 MB (39.60%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        int time = 0;
         unordered_map<int,vector<pair<int,int>>> mp;
         for(int i = 0; i<times.size(); i++){
             int a = times[i][0];
             int b = times[i][1];
             int c = times[i][2];
+
             mp[a].push_back({b,c});
         }
-        vector<bool> vis(n+1,false);
+        vector<int> time(n+1,INT_MAX);
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
         pq.push({0,k});
         while(!pq.empty()){
             auto t = pq.top();
             pq.pop();
-            int w = t.first;
             int node = t.second;
-            if(vis[node]){
+            int cost = t.first;
+            if(time[node] != INT_MAX){
                 continue;
             }
-            vis[node] = true;
-            time = max(time,w);
+            time[node] = cost;
             for(int i = 0; i<mp[node].size(); i++){
                 int child = mp[node][i].first;
-                int child_wt = mp[node][i].second;
-                if(!vis[child]){
-                    pq.push({w+child_wt,child});
-                }
+                int c_cost = mp[node][i].second;
+                pq.push({cost+c_cost,child});
             }
         }
-        for(int i = 1; i<=n; i++){
-            if(!vis[i]){
+        int ans = INT_MIN;
+        for(int i = 1; i<time.size(); i++){
+            if(time[i] == INT_MAX){
                 return -1;
             }
+            ans = max(ans,time[i]);
         }
-        return time;
+        return ans;
     }
 };
