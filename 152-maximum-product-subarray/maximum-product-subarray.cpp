d91d6deb78bc712fc6c9334b454d0030b4ec5a93
Yash Baghel <yashbaghel005@gmail.com>
2026-08-23 11:31:04 +0530

    Time: 145 ms (8.97%) | Memory: 17.7 MB (87.38%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -4,17 +4,17 @@ public:
         int pref = 1;
         int suff = 1;
         int n = nums.size();
-
         int ans = INT_MIN;
         for(int i = 0; i<nums.size(); i++){
-            if(pref == 0){
-                pref = 1;
-            }if(suff == 0){
-                suff =1;
-            }
             pref*=nums[i];
             suff*=nums[n-i-1];
+            cout<<pref<<' '<<suff<<endl;
             ans = max(ans,max(pref,suff));
+            if(pref == 0){
+                pref = 1;
+            }else if(suff == 0){
+                suff = 1;
+            }
         }
         return ans;
     }
